f3ab7a1048c2fddf836713ce35269c4236d674bc
Salahdin Belghiti <salahdin.belghiti@movicoders.com>
2019-12-16 13:02:15 +0100

    Modulo mando terminado y testeado

---
+++ modulo_mando.py
@@ -0,0 +1,22 @@
+from lirc import RawConnection
+
+def teclaMando():
+       
+    #get IR command
+    #keypress format = (hexcode, repeat_num, command_key, remote_id)
+    try:
+        keypress = conn.readline(.0001)
+    except:
+        keypress=""
+              
+    if (keypress != "" and keypress != None):
+                
+        data = keypress.split()
+        sequence = data[1]
+        command = data[2]
+        
+        #ignore command repeats
+        if (sequence != "00"):
+           return
+        
+        return(command)
