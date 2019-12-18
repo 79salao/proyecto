4c97e6796a4e9fc40e80e5b5327e381d7d805dd0
MihaiAurar <57664538+MihaiAurar@users.noreply.github.com>
2019-12-18 09:52:50 +0100

    Update modulo_sonidos.py

---
+++ modulo_sonidos.py
@@ -0,0 +1,29 @@
+
+import time
+import gpiozero
+   
+buzzer = Buzzer(12)
+
+def sonido(color):
+    if color =="blanco":
+        for i in range(8):
+            buzzer.on()
+            time.sleep(0.25)
+            buzzer.off()
+            time.sleep (0.25)
+            
+    elif color =="verde":
+        for i in range(4):
+            buzzer.on()
+            time.sleep(0.5)
+            buzzer.off()
+            time.sleep(0.5)
+        
+    elif color =="amarillo":
+        for i in range(2):
+            buzzer.on()
+            time.sleep(0.75)
+            buzzer.off()
+            time.sleep(0.75)
+        
+
