293624a19fecbc2ffb4bf19ecc9e0a422d9cff37
MihaiAurar <57664538+MihaiAurar@users.noreply.github.com>
2019-12-20 09:52:16 +0100

    Update modulo_email.py
    2 nuevas funciones agregadas

--- modulo_email.py
+++ modulo_email.py
@@ -7,36 +7,45 @@ from email.mime.base import MIMEBase
 from email import encoders
 import os.path
 
+
+mail =''
+
 email = 'esmipruebapy@gmail.com'
 password = 'holamundo1234'
-send_to_email = 'salahdinb99@gmail.com'
+send_to_email = mail
 subject = 'This is the subject'
 message = 'This is my message'
 file_location = modulo_archivos.getPath()
 
-msg = MIMEMultipart()
-msg['From'] = email
-msg['To'] = send_to_email
-msg['Subject'] = subject
+def obtener():
+    mail = input('Escribe email:  ')
+    return mail
 
-msg.attach(MIMEText(message, 'plain'))
+def mandar():
+    global email,password,send_to_email,subject,message,file_location
+    send_to_email=obtener()
+    msg = MIMEMultipart()
+    msg['From'] = email
+    msg['To'] = send_to_email
+    msg['Subject'] = subject
+        
+    msg.attach(MIMEText(message, 'plain'))
 
-# Setup the attachment
-filename = os.path.basename(file_location)
-attachment = open(file_location, "rb")
-part = MIMEBase('application', 'octet-stream')
-part.set_payload(attachment.read())
-encoders.encode_base64(part)
-part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+    filename = os.path.basename(file_location)
+    attachment = open(file_location, "rb")
+    part = MIMEBase('application', 'octet-stream')
+    part.set_payload(attachment.read())
+    encoders.encode_base64(part)
+    part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+  
+    msg.attach(part)
 
-# Attach the attachment to the MIMEMultipart object
-msg.attach(part)
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(email, password)
+    text = msg.as_string()
+    server.sendmail(email, send_to_email, text)
+    server.quit()
+    modulo_archivos.borrarArchivo()
 
-server = smtplib.SMTP('smtp.gmail.com', 587)
-server.starttls()
-server.login(email, password)
-text = msg.as_string()
-server.sendmail(email, send_to_email, text)
-server.quit()
-modulo_archivos.borrarArchivo()
-
+mandar()
